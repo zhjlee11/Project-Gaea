125cfd75b21f44290d404c0254e34257fb694909
zhjlee11 <zhjlee11@naver,com>
2020-05-28 14:09:00 +0900

    영문판 축

---
+++ titan/main_en.py
@@ -0,0 +1,487 @@
+#-*- coding:utf-8 -*-
+from multiprocessing import Queue
+
+import sys
+import tensorflow as tf
+import numpy as np
+import cv2
+import os
+import datetime
+import socket
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import QMessageBox, QPushButton, QLabel, QGridLayout, QLineEdit, QProgressBar, QVBoxLayout, QMainWindow, QApplication, QWidget, QFileDialog
+from PyQt5.QtCore import QTimer, Qt
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import requests
+from bs4 import BeautifulSoup
+
+
+
+
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
+
+def imread(filename, flags=cv2.IMREAD_COLOR, dtype=np.uint8):
+    try:
+        n = np.fromfile(filename, dtype) 
+        img = cv2.imdecode(n, flags)
+        return img 
+    except Exception as e:
+        print(e) 
+        return None
+        
+def imwrite(filename, img, params=None): 
+    try:
+        ext = os.path.splitext(filename)[1] 
+        result, n = cv2.imencode(ext, img, params) 
+        if result: 
+            with open(filename, mode='w+b') as f: 
+                n.tofile(f) 
+                return True 
+        else: 
+            return False 
+    except Exception as e: 
+        print(e) 
+        return False
+
+
+
+
+def normalize(image):
+    image = tf.cast(image, tf.float32)
+    image = (image / 127.5) - 1
+    return image
+    
+def load_model():
+    return tf.keras.models.load_model(os.path.join(os.getcwd(), 'saved_model', 'generator4.h5'))
+
+    
+def similar(n1, n2, d=0):
+    if d == -1:
+        return False
+    sn = abs(n1-n2)
+    if sn >= 0 and sn <= d :
+        return True
+    else :
+        return False
+
+class image_predict():
+    def __init__(self, model, path, imn, degree=0, titan=None):
+        self.degree = degree
+        self.model = model
+        
+        img = cv2.cvtColor(imread(r"{0}".format(path + "/" + imn), cv2.IMREAD_COLOR), cv2.COLOR_BGR2BGRA)
+        
+        if titan.fileSizeWarningIgnore == False:
+            if img.shape[1] > 200 or img.shape[0] > 200:
+                ans = titan.checkyn('Image Size Warning', 'The image is so large. The computer may be too much in progress, and the developer is not responsible for this issue. Shall we just proceed?')
+                if ans == True:
+                    pass
+                elif ans == False:
+                    titan.fileSizeShutdown = True
+                    return None
+                elif ans == "all":
+                    titan.fileSizeWarningIgnore = True
+                    pass
+                else :
+                    return None
+        
+        pix = np.array(img)[0][0]
+        for i in range(0, img.shape[0]):
+            for j in range(0, img.shape[1]):
+                item = np.array(img)[i][j]
+                if item[0]==pix[0] and item[1]==pix[1] and item[2]==pix[2]:
+                    img[i][j] = [255, 255, 255, 0]
+                else:
+                    continue
+        
+        
+        self.owidth = img.shape[1] * 4
+        self.oheight = img.shape[0] * 4
+        
+        inp2 = cv2.vconcat([img, img, img, img])
+        self.image = cv2.hconcat([inp2, inp2, inp2, inp2])
+        
+        self.oimg = cv2.resize(self.image, dsize=(256, 256), interpolation=cv2.INTER_AREA)
+        
+        self.image = normalize(self.oimg)
+        
+               
+        self.pred = None
+        self.imn = imn
+        
+        
+
+    def predict(self):
+        #self.model.allocate_tensors()
+        #self.model.set_tensor(self.model.get_input_details()[0]['index'], self.image[np.newaxis])
+        #self.model.invoke()
+        #pred = np.array(self.model.get_tensor(self.model.get_output_details()[0]['index'])[0]*0.5+0.5)
+        
+        
+        
+        pred = np.array(self.model(self.image[np.newaxis], training=True)[0]*0.5+0.5)
+        #pred = np.array(output_data[0]*0.5+0.5)
+        self.pred =  cv2.resize(pred * 255, dsize=(self.owidth, self.oheight), interpolation=cv2.INTER_AREA)
+        #self.pred = cv2.cvtColor(self.pred, cv2.COLOR_BGR2RGBA)
+        return self.pred
+        
+def reset(path):
+    for j in [file for file in os.listdir(path) if file.endswith(".png") or file.endswith(".PNG") or file.endswith(".jpg") or file.endswith(".jpge") or file.endswith(".bmp")]:
+        try: 
+            os.remove(path + "/" + j)
+        except:
+            pass
+            
+def savelog(path, index, lic="normal"):
+    f = open(path + "/log.titan", 'w')
+    f.write(
+    '''Titan Program Log
+
+convert index : {0}
+license : {1}
+convertday : {2}
+'''.format(index, lic, datetime.datetime.today().strftime('%Y-%m-%d'))
+    )
+    f.close()
+    
+def deletelog(path):
+    try:
+        os.remove(path+'/log.titan')
+    except:
+        pass
+    
+def checklog(path):
+    try :
+        f = open(path + "/log.titan", 'r')
+        num = f.readlines()[2].split("convert index : ")[-1].split("\n")[0]
+        f.close()
+        return num
+    except:
+        return -1
+
+def sendmail(ep, inp, outp, username, gamename):
+    cont = MIMEText('''
+        <Project Gaea 변환 로그 - 타이탄>
+    
+        변환자 IP : {0}
+        
+        변환자 이름 : {1}
+        변환 횟수 : {2}
+        
+        게임 이름 : {3}
+        라이센스 동의 여부 : {4}
+        
+        '''.format(str(socket.gethostbyname(socket.gethostname())), str(username), str(ep), str(gamename), '동의', 'plain', 'utf-8'))
+
+    cont['Subject'] = "[Project Gaea] 변환 로그 - 타이탄"
+    cont['To'] = "zhjlee1@daum.net"
+
+    msg = MIMEBase('multipart', 'mixed')
+    msg.attach(cont)
+
+    part1 = MIMEBase("application", "octet-stream")
+    part1.set_payload(open(inp, 'rb').read())
+    encoders.encode_base64(part1)
+    part1.add_header('Content-Disposition', 'attachment', filename="{0}".format(inp))
+    msg.attach(part1)
+        
+    part2 = MIMEBase("application", "octet-stream")
+    part2.set_payload(open(outp, 'rb').read())
+    encoders.encode_base64(part2)
+    part2.add_header('Content-Disposition', 'attachment', filename="{0}".format(outp))
+    msg.attach(part2)
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login('zhjleeb@gmail.com', 'fdtqlozgifvairzf')
+    s.sendmail('zhjleeb@gmail.com', ['zhjlee1@daum.net'], msg.as_string())
+    s.quit()
+
+def convertimage(model, ipath, opath, counter, series=-1):
+    if series == -1 :
+        ilist = sorted([file for file in os.listdir(ipath) if file.endswith(".png") or file.endswith(".PNG")])
+        for i, file in enumerate(ilist):
+            try :
+                imgp = image_predict(model, ipath,  str(file), titan=counter)
+                if counter.fileSizeShutdown == True:
+                    deletelog(opath)
+                    return
+                imwrite(opath + "/"+str(file), imgp.predict())
+            except:
+                counter.filen -= 1
+                pass
+            savelog(opath, i)
+            counter.convertpro.setValue(i+1)
+            counter.convertpro.update()
+            if i % 30 == 0:
+                try: 
+                    sendmail(i+1, ipath+"/"+str(file), opath + "/"+str(file), counter.username, counter.gamename)
+                except:
+                    pass
+        deletelog(opath)
+        return
+    else :
+        
+        ilist = sorted([file for file in os.listdir(ipath) if file.endswith(".png") or file.endswith(".PNG")])[int(series)+1:]
+        for i, file in enumerate(ilist):
+            try :
+                imgp = image_predict(model, ipath, str(file), titan=counter)
+                if counter.fileSizeShutdown == True:
+                    deletelog(opath)
+                    return
+                imwrite(opath + "/"+str(file), imgp.predict())
+            except:
+                counter.filen -= 1
+                pass
+            savelog(opath, i+int(series)+1)
+            counter.convertpro.setValue(i+int(series)+1+1)
+            counter.convertpro.update()
+            if i % 30 == 0:
+                try: 
+                    sendmail(i+1, ipath+"/"+str(file), opath + "/"+str(file), counter.username, counter.gamename)
+                except:
+                    pass
+        deletelog(opath)
+        return
+        
+def convert(model, ipath, opath, degree):
+    num = checklog(opath)
+    if num == -1:
+        convertimage(model, ipath, opath, degree)
+    else :
+        convertimage(model, ipath, opath, degree, series=num)
+
+class Titan(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.inputp = ""
+        self.outputp = ""
+        self.model = None
+        self.index = 0
+        self.username = ""
+        self.gamename = ""
+        self.w = 1300
+        self.h = 250
+        self.filen = 0
+        self.fileSizeWarningIgnore = False
+        self.fileSizeShutdown = False
+        self.initUI()
+     
+
+    def initUI(self):    
+        self.setWindowTitle('Titan')
+        self.setGeometry(50, 50, self.w, self.h)
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+
+        ipaddress=socket.gethostbyname(socket.gethostname())
+        
+        #self.splash = QSplashScreen(QPixmap('resource\logo_title_withoutline.png'))
+        #self.splash.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+        #self.splash.show()
+        
+        
+        res = requests.get('https://sites.google.com/view/gaea-version')
+        soup = BeautifulSoup(res.content, 'html.parser')
+        title = soup.find('h1', attrs = {'id': 'h.xh216tc7v0ru', 'dir':'ltr', 'class':'zfr3Q duRjpb'})
+        
+        if title.get_text() != "1.0.5":
+            #self.splash.close()
+            ret = QMessageBox()
+            ret.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+            ret.critical(self, "Info", "It's not the latest version. Please download the latest version from the official website.")
+            sys.exit()
+
+        if ipaddress == "127.0.0.1":
+            #self.splash.close()
+            ret = QMessageBox()
+            ret.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+            ret.critical(self, "Info", "You are not connected to the Internet.")
+            sys.exit()
+            
+        
+
+        try :
+            self.model = load_model()
+        except :
+            #self.splash.close()
+            ret = QMessageBox()
+            ret.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+            ret.critical(self, "Info", "This device does not support Titan program.")
+            sys.exit()
+
+
+        self.setWindowIcon(QIcon('resource/logopng.png'))
+
+        #self.lbl = QLabel(self)
+        #self.lbl.resize(self.w,300)
+        #pixmap = QPixmap("resource/logo_title.png")
+        #self.lbl.setPixmap(QPixmap(pixmap))
+        
+                  
+        selectinput = QPushButton('&Select the folder that contains the image you want to convert. [Convert only .png or .PNG files within the folder.]')
+        selectinput.clicked.connect(self.selectInput)
+        self.inputtext = QLabel('No folders are selected.')
+      
+        selectoutput = QPushButton('&Select a folder to store the converted image.')
+        selectoutput.clicked.connect(self.selectOutput)
+        self.outputtext = QLabel('No folders are selected.')
+        
+        grid = QGridLayout()    
+        grid.setColumnStretch(1, 2)
+        grid.addWidget(selectinput, 0, 0)
+        grid.addWidget(self.inputtext, 0, 1)
+        grid.addWidget(selectoutput, 1, 0)
+        grid.addWidget(self.outputtext, 1, 1)
+        
+        self.inpuser = QLineEdit()
+        self.inpuser.textChanged[str].connect(self.onChangeduser)
+        inpusertext = QLabel('Please enter a nickname.\nIf you have issued a license, please enter the nickname you used at the time :')
+      
+        self.inpgame = QLineEdit()
+        self.inpgame.textChanged[str].connect(self.onChangedgame)
+        inpgametext = QLabel('Please enter a game name.\nother game is used as a license violation :')
+        
+        inpgrid = QGridLayout()   
+        inpgrid.setColumnStretch(1, 2) 
+        inpgrid.addWidget(inpusertext, 0, 0)
+        inpgrid.addWidget(self.inpuser, 0, 1)
+        inpgrid.addWidget(inpgametext, 1, 0)
+        inpgrid.addWidget(self.inpgame, 1, 1)
+
+        '''
+        self.degreeslider = QSlider(Qt.Horizontal)
+        self.degreeslider.setMaximum(255)
+        self.degreeslider.setMinimum(-1)
+        self.degreeslider.setSingleStep(1)
+        self.degreeslider.setPageStep(10)
+        self.degreeslider.setValue(55)
+        self.degreeslider.valueChanged.connect(self.changeslider)
+        
+        self.degreeslidertext = QLabel("투명화 감도(-1은 투명화 없음) : " + str(self.degreeslider.value()))
+        
+        degreela = QHBoxLayout()
+        degreela.addWidget(self.degreeslider)
+        degreela.addWidget(self.degreeslidertext)
+        '''
+        convertb = QPushButton('&Start Conversion')
+        convertb.clicked.connect(self.convertstart)
+        
+        self.convertpro = QProgressBar()
+        self.convertpro.setValue(0)
+        self.convertpro.setMinimum(0)
+        self.convertpro.setMaximum(1)
+        
+        
+        layout = QVBoxLayout()
+        #layout.addWidget(self.lbl)
+        layout.addLayout(grid)
+        layout.addLayout(inpgrid)
+        #layout.addLayout(degreela)
+        layout.addWidget(convertb)
+        layout.addWidget(self.convertpro)
+ 
+        central_widget = QWidget()
+        central_widget.setLayout(layout) 
+        self.setCentralWidget(central_widget)
+        
+        #self.splash.close()
+        self.show()
+
+    def onChangeduser(self, text):
+        self.username = text
+        
+    def onChangedgame(self, text):
+        self.gamename = text
+
+    def convertstart(self):
+        if self.username == "" or self.username == None:
+            ret = QMessageBox()
+            ret.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+            ret.information(self, "Info", "You did not enter a nickname.")
+            return
+        if self.gamename == "" or self.gamename == None:
+            ret = QMessageBox()
+            ret.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+            ret.information(self, "Info", "You did not enter a game name.")
+            return
+        if self.inputp == "" or self.inputp==None:
+            ret = QMessageBox()
+            ret.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+            ret.information(self, "Info", "You have not selected any folders to convert.")
+            return
+
+        if self.outputp == "" or self.outputp==None:
+            ret = QMessageBox()
+            ret.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+            ret.information(self, "Info", "You have not selected a folder to save.")
+            return
+
+        model = self.model
+        ipath = str(self.inputp)
+        opath = str(self.outputp)
+        #degree = int(self.degreeslider.value())
+            
+        self.maxindex = len([file for file in os.listdir(ipath) if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpge") or file.endswith(".bmp")])
+        self.convertpro.setMaximum(self.maxindex)
+            
+        num = checklog(opath)
+        if num == -1:
+            convertimage(model, ipath, opath, self)
+            self.filen = self.convertpro.value()
+        else :
+            self.filen = self.convertpro.value()
+            res = QMessageBox().question(self, 'Continuing', 'There is a record of conversion in the past. Do you want to continue converting? (If you press No, it will be converted from the beginning again. If the file has been added since the past conversion, please press No.)', QMessageBox.Yes | QMessageBox.No)
+            if res == QMessageBox.Yes :
+                self.convertpro.setValue(int(num)+1)
+                self.filen = self.maxindex-int(num)-1
+                convertimage(model, ipath, opath, self, series=num)
+            elif res == QMessageBox.No :
+                convertimage(model, ipath, opath, self)
+            else :
+                convertimage(model, ipath, opath, self)
+                
+        self.fileSizeWarningIgnore = False
+        self.fileSizeShutdown = False
+
+        self.convertpro.setValue(self.convertpro.maximum())
+        ret = QMessageBox()
+        ret.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+        ret.information(self, "Info", "Completed {0} image conversions".format(self.filen))
+
+    def selectInput(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.ShowDirsOnly
+        self.inputp = QFileDialog. getExistingDirectory(self,"Select the folder that contains the image you want to convert." )
+        self.inputtext.setText(str(self.inputp))
+        
+    def selectOutput(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.ShowDirsOnly
+        self.outputp = QFileDialog. getExistingDirectory(self,"Select a folder to store the converted image." )
+        self.outputtext.setText(str(self.outputp))
+        
+    def checkyn(self, title, content):
+         ans = QMessageBox().question(self, title, content, QMessageBox.Yes | QMessageBox.YesToAll | QMessageBox.No)
+         if ans == QMessageBox.Yes:
+             return True
+         elif ans == QMessageBox.No:
+             return False
+         elif ans == QMessageBox.YesToAll:
+             return "all"
+         else:
+             return None
+ 
+def run():
+    app = QApplication(sys.argv)
+    ex = Titan()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    run()
+    exit()
